7c9e463a14a02c80e07cdad11b4d821023dc4cb0
zmitr-gen <zmitr-gen@yandex.ru>
2017-07-23 23:33:20 +0300

    Logic of GCD was refactored using delegates.

--- Task1.Logic/SearcherGCD.cs
+++ Task1.Logic/SearcherGCD.cs
@@ -23,15 +23,14 @@ namespace Task1.Logic
         /// </returns>
         public static long SearchByEuclid(long a, long b)
         {
-            long temp;
             while (a != 0)
             {
-                temp = a;
+                long temp = a;
                 a = b % a;
                 b = temp;
             }
 
-            return (Math.Abs(b));
+            return Math.Abs(b);
         }
 
         /// <summary>
@@ -42,17 +41,16 @@ namespace Task1.Logic
         /// GCD of two number in long representation.
         /// </returns>
         /// <param name="time"> The algorythm running time in long representation. </param>
-        public static long SearchByEuclid(out long time, long a, long b)
-        {
-            var sw = Stopwatch.StartNew();
-
-            long gcd = SearchByEuclid(a, b);
-
-            time = sw.ElapsedTicks;
+        public static long SearchByEuclid(out long time, long a, long b) => GCD(SearchByEuclid, a, b, out time);
 
 
-            return gcd;
-        }
+        /// <summary>
+        /// Computes GCD of three numbers by classical Euclidian algorithm.
+        /// </summary>
+        /// <returns>
+        /// GCD of two number in long representation.
+        /// </returns>
+        public static long SearchByEuclid(long a, long b, long c) => GCD(SearchByEuclid, a, b, c);
 
         /// <summary>
         /// Computes GCD of three numbers by classical Euclidian algorithm.
@@ -61,18 +59,16 @@ namespace Task1.Logic
         /// GCD of two number in long representation.
         /// </returns>
         /// <param name="time"> The algorythm running time in long representation. </param>
-        public static long SearchByEuclid(out long time, long a, long b, long c)
-        {
-            var sw = Stopwatch.StartNew();
-
-            long gcd = SearchByEuclid(a, b);
-            gcd = SearchByEuclid(gcd, c);
-
-            time = sw.ElapsedTicks;
+        public static long SearchByEuclid(out long time, long a, long b, long c) => GCD(SearchByEuclid, a, b, c, out time);
 
 
-            return gcd;
-        }
+        /// <summary>
+        /// Computes GCD of some numbers by classical Euclidian algorithm.
+        /// </summary>
+        /// <returns>
+        /// GCD of two number in long representation.
+        /// </returns>
+        public static long SearchByEuclid(params long[] nums) => GCD(SearchByStein, nums);
 
         /// <summary>
         /// Computes GCD of some numbers by classical Euclidian algorithm.
@@ -81,23 +77,8 @@ namespace Task1.Logic
         /// GCD of two number in long representation.
         /// </returns>
         /// <param name="time"> The algorythm running time in long representation. </param>
-        public static long SearchByEuclid(out long time, params long[] nums)
-        {
-            CheckInputArray(nums);
-
-            var sw = Stopwatch.StartNew();
-
-            long gcd = SearchByEuclid(nums[0], nums[1]);
-
-            for (int i = 2; i < nums.Length; i++)
-            {
-                gcd = SearchByEuclid(gcd, nums[i]);
-            }
-
-            time = sw.ElapsedTicks;
-
-            return gcd;
-        }
+        /// <param name="nums"> Array of nums. </param>
+        public static long SearchByEuclid(out long time, params long[] nums) => GCD(SearchByStein, out time, nums);
 
         #endregion
 
@@ -125,10 +106,12 @@ namespace Task1.Logic
                 return Math.Abs((long)gcd);
 
             if ((a & 1) == 0)
-                gcd = ((b & 1) == 0) ? SearchByStein(a >> 1, b >> 1) << 1
+                gcd = ((b & 1) == 0)
+                    ? SearchByStein(a >> 1, b >> 1) << 1
                     : SearchByStein(a >> 1, b);
             else
-                gcd = ((b & 1) == 0) ? SearchByStein(a, b >> 1)
+                gcd = ((b & 1) == 0)
+                    ? SearchByStein(a, b >> 1)
                     : SearchByStein(b, a > b ? a - b : b - a);
 
             return Math.Abs((long)gcd);
@@ -140,59 +123,37 @@ namespace Task1.Logic
         /// </summary>
         /// <returns> GCD of two number in long representation. </returns>
         /// <param name="time"> The algorythm running time in long representation. </param>
-        public static long SearchByStein(out long time, long a, long b)
-        {
-            var sw = Stopwatch.StartNew();
-
-            long gcd = SearchByStein(a, b);
-
-            time = sw.ElapsedTicks;
+        public static long SearchByStein(out long time, long a, long b) => GCD(SearchByStein, a, b, out time);
 
 
-            return gcd;
-        }
+        /// <summary>
+        /// Computes GCD of three numbers by Stein's algorithm.
+        /// </summary>
+        /// <returns> GCD of two number in long representation. </returns>
+        public static long SearchByStein(long a, long b, long c) => GCD(SearchByStein, a, b, c);
 
         /// <summary>
         /// Computes GCD of three numbers by Stein's algorithm.
         /// </summary>
         /// <returns> GCD of two number in long representation. </returns>
         /// <param name="time"> The algorythm running time in long representation. </param>
-        public static long SearchByStein(out long time, long a, long b, long c)
-        {
-            var sw = Stopwatch.StartNew();
+        public static long SearchByStein(out long time, long a, long b, long c) =>
+            GCD(SearchByStein, a, b, c, out time);
 
-            long gcd = SearchByStein(a, b);
-            gcd = SearchByStein(gcd, c);
-
-            time = sw.ElapsedTicks;
-
-            return gcd;
-        }
 
         /// <summary>
         /// Computes GCD of some numbers by Stein's algorithm.
         /// </summary>
         /// <returns> GCD of two number in long representation. </returns>
         /// <param name="time"> The algorythm running time in long representation. </param>
-        public static long SearchByStein(out long time, params long[] nums)
-        {
-            CheckInputArray(nums);
+        public static long SearchByStein(params long[] nums) => GCD(SearchByStein, nums);
 
-            var sw = Stopwatch.StartNew();
-
-
-            long gcd = SearchByStein(nums[0], nums[1]);
-
-            for (int i = 2; i < nums.Length; i++)
-            {
-                gcd = SearchByStein(gcd, nums[i]);
-            }
-
-
-            time = sw.ElapsedTicks;
-
-            return gcd;
-        }
+        /// <summary>
+        /// Computes GCD of some numbers by Stein's algorithm.
+        /// </summary>
+        /// <returns> GCD of two number in long representation. </returns>
+        /// <param name="time"> The algorythm running time in long representation. </param>
+        public static long SearchByStein(out long time, params long[] nums) => GCD(SearchByStein, out time, nums);
 
         #endregion
 
@@ -206,6 +167,60 @@ namespace Task1.Logic
 
             if (nums.Length < 2)
                 throw new ArgumentException();
+        }
+
+        private static long GCD(Func<long, long, long> gcdFunc, long a, long b, out long time)
+        {
+            var sw = Stopwatch.StartNew();
+
+            long gcd = gcdFunc(a, b);
+
+            time = sw.ElapsedTicks;
+
+
+            return gcd;
+        }
+
+
+        private static long GCD(Func<long, long, long> gcdFunc, long a, long b, long c) => gcdFunc(gcdFunc(a, b), c);
+
+        private static long GCD(Func<long, long, long, long> gcdFunc, long a, long b, long c, out long time)
+        {
+            var sw = Stopwatch.StartNew();
+
+            long gcd = gcdFunc(a, b, c);
+
+            time = sw.ElapsedTicks;
+
+
+            return gcd;
+        }
+
+
+        private static long GCD(Func<long, long, long> gcdFunc, params long[] nums)
+        {
+            CheckInputArray(nums);
+
+            long gcd = gcdFunc(nums[0], nums[1]);
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                gcd = gcdFunc(gcd, nums[i]);
+            }
+
+            return gcd;
+        }
+
+        private static long GCD(Func<long[], long> gcdFunc, out long time, params long[] nums)
+        {
+            var sw = Stopwatch.StartNew();
+
+            long gcd = gcdFunc(nums);
+
+            time = sw.ElapsedTicks;
+
+
+            return gcd;
         }
 
         #endregion
